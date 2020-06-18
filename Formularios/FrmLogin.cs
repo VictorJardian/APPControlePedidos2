139aa85936b22e4b3b711a16432ae3774fbb8d44
Victor Jardian Coelho de Paiva <victorjardiam@gmail.com>
2020-06-18 17:32:50 -0300

    SALVANDO

--- Formularios/FrmLogin.cs
+++ Formularios/FrmLogin.cs
@@ -13,6 +13,9 @@ namespace APPControlePedidos2
 {
     public partial class FrmLogin : Form
     {
+        static string usuario;
+        static string senha;
+
         public FrmLogin()
         {
             InitializeComponent();
@@ -31,8 +34,54 @@ namespace APPControlePedidos2
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            FrmTelaPrincipal frm = new FrmTelaPrincipal();
-            frm.Show();
+            try
+            {
+                string SQL = "select * FROM Logins where Usuario=@Usuario and Senha=@Senha";
+                String conexao = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\win10\Desktop\BandoDadosAcessPROJETO02.mdb";
+                OleDbConnection con = new OleDbConnection(conexao);
+                con.Open();
+
+                OleDbCommand cmd = new OleDbCommand(SQL, con);
+
+                cmd.Parameters.AddWithValue("@Usuario",txtUsuario.Text);
+                cmd.Parameters.AddWithValue("@Senha", txtSenha.Text);
+
+                OleDbDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    autenticacao(dr["Usuario"].ToString(), dr["Senha"].ToString());
+                    con.Close();
+                    MessageBox.Show("Logado com sucesso!");
+                    FrmTelaPrincipal frm = new FrmTelaPrincipal();
+                    frm.Show();
+
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Erro ao logar!");
+                }
+
+            }
+
+            catch (Exception erro)
+            {
+                MessageBox.Show(erro.Message);
+
+            }
+
+        }  
+
+        public static void autenticacao(string usuario1, string senha1)
+        {
+            usuario = usuario1;
+            senha = senha1;   
         }
+        public static String GetUsuario()
+        {
+            return "Usuario: " + usuario + "\nSenha: " + senha;
+        }
+
     }
 }
